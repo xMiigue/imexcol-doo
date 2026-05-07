1693e4e95c6877a5f46c24742be2099432c2c31f
xMiigue <miguelalbe2509@gmail.com>
2026-05-07 16:22:11 -0500

    feat: Inicialización del proyecto extra clase Spring Boot Imexcol

---
+++ src/main/java/co/edu/uco/imexcol/inicializador/ImexcolApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.imexcol.inicializador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImexcolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImexcolApplication.class, args);
+	}
+
+}
