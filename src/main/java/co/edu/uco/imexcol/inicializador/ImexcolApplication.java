7665f912acc323fe83d28ae201600206aea5e74a
xMiigue <miguelalbe2509@gmail.com>
2026-05-14 01:28:07 -0500

    Fix: agrega ComponentScan en ImexcolApplication para que Spring Boot detecte los controladores REST

--- src/main/java/co/edu/uco/imexcol/inicializador/ImexcolApplication.java
+++ src/main/java/co/edu/uco/imexcol/inicializador/ImexcolApplication.java
@@ -2,7 +2,9 @@ package co.edu.uco.imexcol.inicializador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"co.edu.uco.imexcol"})
 @SpringBootApplication
 public class ImexcolApplication {
 
